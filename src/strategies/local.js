6628b928d0fa2bed100cf05fa38843852c495912
WYC401 <545301030@qq.com>
2023-01-04 00:17:48 -0800

    add serialize and deserialize

--- src/strategies/local.js
+++ src/strategies/local.js
@@ -1,6 +1,9 @@
 const passport = require("passport");
 const {Strategy} = require("passport-local");
 const User = require("../database/schemas/User.js");
+
+// to uderstand the flow of authentication, see http://toon.io/understanding-passportjs-authentication-flow/
+
 passport.use(new Strategy(
    async (email, password, done) => {
         
@@ -20,3 +23,20 @@ passport.use(new Strategy(
         
     }
 ));
+
+passport.serializeUser((user,done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+    try{
+        const user = await User.findById(id);
+        if(!user){
+            throw new Error("User not  find");
+        }
+        done(null, user);
+    } catch(error) {
+        console,log(error);
+        done(error, null);
+    }
+});
