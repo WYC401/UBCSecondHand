b8d364a007b0eda19005bbc769d54ae4026d95d1
WYC401 <545301030@qq.com>
2023-01-13 00:00:34 -0800

    find the way to write the search route

--- src/routes/items.js
+++ src/routes/items.js
@@ -1,6 +1,7 @@
 const ItemDB = require("../database/schemas/Item");
 const Router = require("express");
 const router = Router();
+const mongoose = require('mongoose');
 
 // {
 //     "title": "E-bike",
@@ -9,6 +10,8 @@ const router = Router();
 //     "picturePath" : "/user/pic/1.png"
 // }
 
+
+// these four are from user side.
 router.post("/postNewItem", (req, res) => {
     const temp = req.body;
     temp.userID = req.user._id;
@@ -20,8 +23,9 @@ router.post("/postNewItem", (req, res) => {
     });
 });
 
+//localhost:3000/api/items/63bfb4f997bd709b83cd3774
 router.put("/:itemID", (req, res) => {
-    ItemDB.updateOne({_id: req.params.itemID}, req.body).then(() => {
+    ItemDB.updateOne({_id: mongoose.Types.ObjectId(req.params.itemID)}, req.body).then(() => {
         res.send(200);
     }).catch((err) => {
         console.log(err.message);
@@ -41,4 +45,39 @@ router.get("/", (req, res) => {
     });
 })
 
+router.delete("/:itemID", (req, res) => {
+    ItemDB.deleteOne({_id: mongoose.Types.ObjectId(req.params.itemID)}).then(() => {
+        res.send(200);
+    }).catch((err) => {
+        console.log(err.message);
+        res.send(400);
+    });
+});
+
+
+
+
+
+//from searech side
+// app.get('/users', (req, res) => {
+//     let age = req.query.age;
+//     let operator;
+//     if(age.startsWith('gt')){
+//       operator = {$gt : parseInt(age.slice(2))};
+//     }else if(age.startsWith('lt')){
+//       operator = {$lt : parseInt(age.slice(2))};
+//     }
+//     // Perform the database query to retrieve the users
+//     User.find({age: operator}, (err, users) => {
+//       if (err) {
+//         res.status(500).send(err);
+//       } else {
+//         res.json(users);
+//       }
+//     });
+//   });
+//TODO: write a parse function which return an query object and be passed into find.
+router.get("/search", (req, res) => {
+    console.log(req.query);
+});
 module.exports = router;
