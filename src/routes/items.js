cfcbc3874e953f717aa869a4cf5e28cfb56911cd
WYC401 <545301030@qq.com>
2023-01-15 00:43:59 -0800

    implement the keyword search and I hope I can sort it by relevance.

--- src/routes/items.js
+++ src/routes/items.js
@@ -2,6 +2,9 @@ const ItemDB = require("../database/schemas/Item");
 const Router = require("express");
 const router = Router();
 const mongoose = require('mongoose');
+const fs = require('fs');
+const {open} = require('node:fs/promises');
+const Item = require("../database/schemas/Item");
 
 // {
 //     "title": "E-bike",
@@ -77,7 +80,103 @@ router.delete("/:itemID", (req, res) => {
 //     });
 //   });
 //TODO: write a parse function which return an query object and be passed into find.
-router.get("/search", (req, res) => {
-    console.log(req.query);
+//https://umbraco.com/knowledge-base/http-status-codes/
+router.get("/search", async (req, res) => {
+    console.log( req.query);
+    const parsedQuery = parseParams(req.query);
+    let temp = ItemDB.find(parsedQuery);
+    if(req.query.keywords) {
+        const cleanedKeywords = await breakIntoKeyword(req.query.keywords);
+
+        const keywordConditon = createKeyWordCondition(cleanedKeywords);
+        console.log(keywordConditon["$or"][1]);
+        temp = temp.where(keywordConditon);
+    } 
+
+    temp.exec( (err, items) => {
+        if(err) res.send(400);
+        else {
+            items.forEach (item => {JSON.stringify(item)});
+            console.log(items);
+            res.send(items);
+        }
+    });
 });
+
+// $or: [{title : {$regex: ".*"+word1+".*", $options:"i"}}, {description : {$regex: ".*"+word1+".*", $options:"i"}}]
+
+function createKeyWordCondition(cleanedKeywords) {
+    const result = {};
+    result["$or"] = [];
+    for(let i in cleanedKeywords) {
+        
+        let tempTitle = {};
+        let tempDescription = {}; 
+        tempTitle["title"] = {};
+        tempTitle["title"]["$regex"] = ".*" + cleanedKeywords[i] + ".*";
+        tempTitle["title"]["$options"] = "i";
+        console.log(tempTitle);
+        tempDescription["description"] = {};
+        tempDescription["description"]["$regex"] = ".*" + cleanedKeywords[i] + ".*";
+        tempDescription["description"]["$options"] = "i";
+
+        result["$or"].push(tempDescription);
+        result["$or"].push(tempTitle);
+
+    }
+    return result;
+
+}
+
+function parseParams(rawObject) {
+    let res = {};
+    if(rawObject.hasOwnProperty("category")) {
+        res["category"] = rawObject["category"];
+    }
+    if(rawObject.hasOwnProperty("price")) {
+        // console.log(typeof rawObject["price"][0]);
+        res["price"] = {};
+        if(typeof rawObject["price"] === "string") {
+            createFilterObject(rawObject["price"], res["price"]);
+        } else {
+            rawObject["price"].forEach(element => {
+                createFilterObject(element, res["price"]);
+            });
+        }
+    }
+
+    return res;
+}
+
+//add $ls or $gt to query object 
+function createFilterObject(str, queryObject) {
+    queryObject["$" + str.substring(0,2)] = parseFloat(str.substring(2));
+
+}
+
+async function breakIntoKeyword(keystring) {
+    let cleanedString = keystring.replace(/\s+/g, ' ').trim();
+    let words = cleanedString.split(" ");
+    let stopWords;
+
+    let filehandle;
+
+    try {
+        filehandle = await open('src/configuration/stopwords.txt', 'r');
+        return filehandle.readFile(encoding = 'utf-8').then((str) => {
+            let stopWords = str.split("\r\n");
+            return words.filter(word => !stopWords.includes(word));
+        }).catch((err) => {
+            throw err;
+        });
+    } catch(err) {
+        console.log(err);
+    }finally {
+        await filehandle?.close();
+ 
+    }
+    
+
+
+}
 module.exports = router;
